8db9aa11391039422a2ec46142d4b2eda5bb3fdf
CHIANG WEI <a05110511t@gmail.com>
2016-09-13 00:28:13 +0800

    Update Palindrome.py
    add some commit

--- MMA-CTF-2nd-2016/Palindrome.py
+++ MMA-CTF-2nd-2016/Palindrome.py
@@ -27,19 +27,21 @@ for case in range(0, 30):
     words = client_file.readline().split()[2:]
     # words: input
     # answer: answer
-    ##  Please write some code here
+    ##  Please write some code here 
+    #以上為官方提供與Server互動的source code
     test = words
-    def rotate(l,n):
-        return l[n:] + l[:n]
-    print len(''.join(words))
+    #def rotate(l,n):
+    #    return l[n:] + l[:n]
+    #print len(''.join(words))
     for i in range(0, 7257601):
 	#print 'random:'
-	shuffle(words)
+	shuffle(words) #將list的元素排序
 #	s = sorted(words, key=lambda x: x, reverse = True)
 #	rotate(words, i)
 	s = ''.join(words)
+	#先判斷長度為奇數或是偶數
         if(len(s)%2 == 1):
-            if( s[:len(s)/2:] == s[len(s)/2+1:][::-1] ):
+            if( s[:len(s)/2:] == s[len(s)/2+1:][::-1] ): #直到判斷屬於回文
                 Found = ' '.join(s)
                 print 'Found'
 		break
@@ -48,6 +50,8 @@ for case in range(0, 30):
                 Found = ' '.join(s)
        	        print 'Found'
 		break
+    
+    #以下為官方提供與Server互動的source code
     answer = words
     print  ' '.join(answer)
 
